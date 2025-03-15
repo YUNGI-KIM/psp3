905c26eddca3bd9f24890b53009d29fa9d629b85
my <hslee9275@gmail.com>
2025-03-15 20:24:00 +0900

    예습?

--- my-app/src/NewPage.js
+++ my-app/src/NewPage.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 function NewPage() {
   return (
     <div>
-      <h1>Q.1</h1>
+      <h1>질문 1 : 난 집에 가고 싶다. </h1>
       <Link to="/">
-        <button>홈으로 이동</button>
+        <button> 그렇다 </button>
+        </Link>
+        <Link>
+        <button>아니다</button>
       </Link>
     </div>
   );
