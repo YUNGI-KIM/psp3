3d2ce59e440c34544e32961846c7ee70fbf0ecb2
my <hslee9275@gmail.com>
2025-03-17 20:36:20 +0900

    tailwind 적용 로딩 없이 페이지 변경 적용??

---
+++ my-app/src/Q5page.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+function Q2Page() {
+  return (
+    
+<div class="flex items-center gap-8">
+    <label class="inline-flex items-center">
+        <input type="radio" name="vehicle" class="w-5 h-5 text-red-600"/>
+            <span class="ml-2 text-gray-700">
+                Car
+            </span>
+        </label>
+        <label class="inline-flex items-center">
+            <input type="radio" name="vehicle" class="w-5 h-5 text-red-600"/>
+                <span class="ml-2 text-gray-700">
+                    Cycle
+                </span>
+            </label>
+        </div>
+  
+
+
+
+  );
+}
+
+export default Q2Page;
