a5894fa3bb10a1fcb797fc468bea889a780ad6b3
my <hslee9275@gmail.com>
2025-03-17 21:00:11 +0900

    테일윈드 적용 디자인 테스트 중

--- my-app/src/Q4page.js
+++ my-app/src/Q4page.js
@@ -4,12 +4,24 @@ function Q2Page() {
   return (
     <div>
       <h1>질문 4 : 난 집에 가고 싶다. </h1>
-      <Link to="/Q5page">
-        <button> 그렇다 </button>
+      <Link to="/Q5page">        
+      <div className="flex space-x-4">
+
+      <button type="button" className="py-2 px-4 flex justify-center items-center bg-red-500 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-35 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full">
+          <svg width="20" height="20" className="mr-2" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1344 1472q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm256 0q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm128-224v320q0 40-28 68t-68 28h-1472q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h427q21 56 70.5 92t110.5 36h256q61 0 110.5-36t70.5-92h427q40 0 68 28t28 68zm-325-648q-17 40-59 40h-256v448q0 26-19 45t-45 19h-256q-26 0-45-19t-19-45v-448h-256q-42 0-59-40-17-39 14-69l448-448q18-19 45-19t45 19l448 448q31 30 14 69z"></path>
+          </svg>
+          아니다
+        </button>
+
+        <button type="button" className="py-2 px-4 flex justify-center items-center bg-green-500 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-35 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full">
+          <svg width="20" height="20" className="mr-2" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1344 1472q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm256 0q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm128-224v320q0 40-28 68t-68 28h-1472q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h427q21 56 70.5 92t110.5 36h256q61 0 110.5-36t70.5-92h427q40 0 68 28t28 68zm-325-648q-17 40-59 40h-256v448q0 26-19 45t-45 19h-256q-26 0-45-19t-19-45v-448h-256q-42 0-59-40-17-39 14-69l448-448q18-19 45-19t45 19l448 448q31 30 14 69z"></path>
+          </svg>
+          그렇다
+        </button>
+        </div>
         </Link>
-        <Link to="/Q5page">
-        <button>아니다</button>
-      </Link>
     </div>
   );
 }
