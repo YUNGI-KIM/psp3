8f9eb92302c8b19a0c7baad25380c1f384c83baf
my <hslee9275@gmail.com>
2025-03-18 14:24:35 +0900

    설문조사 시작 화면 비율 조정 및 문구 조정 1. 배경화면 비율이 정사각형에서 직사각형으로 변경되었습니다 sm:max-w-lg > sm:sm:w-flex 2. 문구 변경

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,7 +1,4 @@
 import { Link } from "react-router-dom";
-// 사각형 길이 커스텀 해서 비율 맞게 하기
-// 색 조정 
-// 문구 조정
 function Home() {
   return (
    
@@ -9,28 +6,36 @@ function Home() {
     <div class="inset-0 z-10 w-full h-screen overflow-y-auto">
         <div class="absolute inset-0 w-full h-full bg-gray-500 opacity-75">
         </div>
-        <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+        <div class="flex items-end justify-center min-h-screen px-4  pt-4 pb-20 text-center sm:block sm:p-0">
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
             </span>
-            <div class="relative inline-block overflow-hidden transition-all transform sm:align-middle sm:max-w-lg" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+            <div class="relative inline-block overflow-hidden transition-all transform sm:align-middle sm:max-w-" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                 <div>
                     <div class="rounded-lg p-8 bg-white shadow">
                         <div class="bg-white dark:bg-gray-800 ">
                             <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+                                
                                 <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+                                    
                                     <span class="block">
-                                        어떤 차를 사야할지 모르시겠다고요?
+                                        어떤 차를 사야할지 고민되시나요?
                                     </span>
+
                                     <span class="block text-indigo-500">
-                                        간단한 CBTI를 통해 추천 자동차를 만나보세요
+                                        쉽고 빠른 CBTI를 통해 
                                     </span>
+
+                                    <span class="block text-indigo-500">
+                                        추천 자동차를 만나보세요!!
+                                    </span>
+
                                 </h2>
+
                                 <div class="lg:mt-0 lg:flex-shrink-0">
                                     <div class="mt-12 inline-flex rounded-md shadow">
                                       <Link to= "/Q1page">
                                         <button type="button" class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                            나만의 차 찾으러 가기 
-                                            
+                                            지금 추천 받기 
                                         </button>
                                         </Link>
                                     </div>
