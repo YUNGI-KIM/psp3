8ba7ef8e6b27b97d0bd33e4072bbcc4858028ba5
my <hslee9275@gmail.com>
2025-03-21 11:13:38 +0900

    수정사항 1. 버튼 디자인 굴곡 통일화

--- my-app/src/CBTI/Home.js
+++ my-app/src/CBTI/Home.js
@@ -35,9 +35,9 @@ function Home() {
                                 </h2>
 
                                 <div class="lg:mt-0 lg:flex-shrink-0">
-                                    <div class="mt-12 inline-flex rounded-md shadow">
+                                    <div class="mt-12 inline-flex rounded-md ">
                                       <Link to= "/Q1page">
-                                        <button type="button" class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                        <button type="button" class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-3xl ">
                                         CBTI 검사 시작하기
                                         </button>
                                         </Link>
