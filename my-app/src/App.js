d82c26ac03054cf456669b6723ae75adeb5affc5
my <hslee9275@gmail.com>
2025-03-25 17:33:04 +0900

    수정 1. 코드 수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./CBTI/Home";
-import Q1page from "./CBTI/Q1page";
+import QuestionPage from "./CBTI/QuestionPage";
 import CBTIResult from "./CBTI/CBTIResult";
 import Login from "./Login/Login";
 import Register from "./Login/Register";
@@ -11,10 +11,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />  {/* 홈 페이지 */}
-        <Route path="/Q1page" element={<Q1page />} />  {/* 질문페이지1*/}
-        <Route path="/login" element={<Login/>} />  {/* 홈 페이지 */}
-        <Route path="/register" element={<Register/>} />  {/* 홈 페이지 */}
+        <Route path="/" element={<Home />} />  {/* 홈 페이지 */}
+        <Route path="/QuestionPage" element={<QuestionPage />} />  {/* 질문페이지1*/}
+        <Route path="/login" element={<Login />} />  {/* 홈 페이지 */}
+        <Route path="/register" element={<Register />} />  {/* 홈 페이지 */}
       </Routes>
     </Router>
   );
--- my-app/src/CBTI/ButtonJS/CBTIButton.js
+++ my-app/src/CBTI/ButtonJS/CBTIButton.js
@@ -3,39 +3,39 @@ function ButtonGroup() {
   return (
     <div className="flex justify-center space-x-4">
 
-        <button
-          type="button"
-          className="py-2 px-4 flex justify-center items-center bg-green-500 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-32 transition ease-in duration-200 text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
+      <button
+        type="button"
+        className="py-2 px-4 flex justify-center items-center bg-green-500 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-32 transition ease-in duration-200 text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
+      >
+        <svg
+          width="20"
+          height="20"
+          className="mr-2"
+          fill="currentColor"
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            width="20"
-            height="20"
-            className="mr-2"
-            fill="currentColor"
-            viewBox="0 0 512 512"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/>
-          </svg>
-          그렇다
-        </button>
+          <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z" />
+        </svg>
+        그렇다
+      </button>
 
-        <button
-          type="button"
-          className="py-2 px-4 flex justify-center items-center bg-red-500 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-32 transition ease-in duration-200 text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
+      <button
+        type="button"
+        className="py-2 px-4 flex justify-center items-center bg-red-500 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-32 transition ease-in duration-200 text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full"
+      >
+        <svg
+          width="20"
+          height="20"
+          className="mr-2"
+          fill="currentColor"
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            width="20"
-            height="20"
-            className="mr-2"
-            fill="currentColor"
-            viewBox="0 0 512 512"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/>
-          </svg>
-          아니다
-        </button>
+          <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
+        </svg>
+        아니다
+      </button>
 
 
     </div>
