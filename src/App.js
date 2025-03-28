08fb85c090913becd3831700547b91cf3e7c61c6
my <hslee9275@gmail.com>
2025-03-28 10:27:07 +0900

    머지 1. CBTIDevelop 브랜치와 main_Develop 브랜치를 머지 후 의존성 문제를 해결하였습니다. 2. 테스트 실행 결과 이상 없이 실행 가능 했음 3. CBTIDevelop에서 사용하던 my-app 파일을 삭제하였음

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
-//이미지 import <img src={logo} />
 import './App.css';
+import StartCBTI from "./CBTI/StartCBTI";
+import QuestionPage from "./CBTI/QuestionPage";
+import CBTIResult from "./CBTI/CBTIResult";
+import Login from "./Login/Login";
+import Register from "./Login/Register";
 
 function App() {
 
@@ -12,6 +16,11 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path = "/" element={<MainPage/>}></Route>
+      <Route path="/startCBTI" element={<StartCBTI/>} />  {/* 스타트 페이지 */}
+        <Route path="/QuestionPage" element={<QuestionPage />} />  {/* 질문페이지들 */}
+        <Route path="/result" element={<CBTIResult />} />  {/* 결과페이지 */}
+        <Route path="/login" element={<Login/>} />  {/* 로그인 페이지 */}
+        <Route path="/register" element={<Register/>} />  {/* 가입 페이지 */}
     </Routes>
     </BrowserRouter>
     </div>
