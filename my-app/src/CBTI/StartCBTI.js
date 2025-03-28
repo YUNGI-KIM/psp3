948c978e31467a04c82182d97974daae2a1dda84
my <hslee9275@gmail.com>
2025-03-28 09:41:13 +0900

    수정사항 1. 개발자 툴에서 보이는 오류 사항들을 삭제     1-1 html에서 div 내부에 name을 clssname으로 변경

--- my-app/src/CBTI/StartCBTI.js
+++ my-app/src/CBTI/StartCBTI.js
@@ -1,37 +1,37 @@
 import { Link } from "react-router-dom";
 function StartCBTI() {
   return (
-<div class="relative">
-    <div class="inset-0 z-10 w-full h-screen overflow-y-auto">
-        <div class="absolute inset-0 w-full h-full bg-gray-500 opacity-75">
+<div className="relative">
+    <div className="inset-0 z-10 w-full h-screen overflow-y-auto">
+        <div className="absolute inset-0 w-full h-full bg-gray-500 opacity-75">
         </div>
-        <div class="flex items-end justify-center min-h-screen px-4  pt-4 pb-20 text-center sm:block sm:p-0">
-            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+        <div className="flex items-end justify-center min-h-screen px-4  pt-4 pb-20 text-center sm:block sm:p-0">
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
             </span>
-            <div class="relative inline-block overflow-hidden transition-all transform sm:align-middle sm:w-flex" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+            <div className="relative inline-block overflow-hidden transition-all transform sm:align-middle sm:w-flex" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                 <div>
-                    <div class="rounded-3xl p-8 bg-white shadow">
-                        <div class="bg-white dark:bg-gray-800 ">
-                            <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+                    <div className="rounded-3xl p-8 bg-white shadow">
+                        <div className="bg-white dark:bg-gray-800 ">
+                            <div className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
                                 
-                                <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+                                <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
                                     
-                                    <span class="block">
+                                    <span className="block">
                                         어떤 차를 사야할지 고민되시나요?
                                     </span>
 
-                                    <span class="block text-indigo-500">
+                                    <span className="block text-indigo-500">
                                         쉽고 빠른 CBTI검사를 통해 
                                     </span>
 
-                                    <span class="block text-indigo-500">
+                                    <span className="block text-indigo-500">
                                         추천 자동차를 만나보세요!!
                                     </span>
                                 </h2>
-                                <div class="lg:mt-0 lg:flex-shrink-0">
-                                    <div class="mt-12 inline-flex rounded-md ">
+                                <div className="lg:mt-0 lg:flex-shrink-0">
+                                    <div className="mt-12 inline-flex rounded-md ">
                                       <Link to= "/QuestionPage">
-                                        <button type="button" class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-3xl ">
+                                        <button type="button" className="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-3xl ">
                                         CBTI 검사 시작하기
                                         </button>
                                         </Link>
