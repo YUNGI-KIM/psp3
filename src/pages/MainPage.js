39d616b6098034bb8039d200a36a987a7deb8b36
Wasppss <lch9243@gmail.com>
2025-03-31 15:53:25 +0900

    MainPage modified

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -27,7 +27,7 @@ const MainPage = () => {
             className="w-full px-4 text-sm rounded-full focus:outline-none"
           />
         </span>
-          <button onClick={goLogin} type="button"className="py-2  bg-slate-50 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-black w-20 transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg ml-70">
+          <button onClick={goLogin} type="button"className="py-2  bg-slate-50 hover:bg-gray-300 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-black w-20 transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg ml-130">
             로그인
           </button>
           <button onClick={goRegister} type="button" className="py-2  bg-gray-900 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-25 transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-20">
@@ -41,7 +41,7 @@ const MainPage = () => {
         <div className="px-8 mx-auto max-w-7xl">
           <div className="flex items-center justify-between h-16">
             <div>
-              <div className="flex items-baseline ml-10 space-x-4">
+              <div className="flex items-baseline ml-10 space-x-50">
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">차량구매</a>
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">부속부품</a>
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">견적</a>
