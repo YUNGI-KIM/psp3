26a16123c2ec5ab9236757b23e30e6cd6d814fbf
my <hslee9275@gmail.com>
2025-04-01 17:13:42 +0900

    업데이트 1. 반응형 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -13,87 +13,78 @@ const MainPage = () => {
   const navigate = useNavigate();
   const [index, setIndex] = useState(0);
 
-  function goLogin() {
-    navigate("/login");
-  }
-
-  function goRegister() {
-    navigate("/register");
-  }
-
   function sliderButton() {
     var num;
     document.querySelectorAll("button").forEach((btn) => {
       btn.addEventListener("click", function (event) {
-          console.log("클릭된 버튼의 ID:", event.target.id);
-          num = event.target.id;
-      
-  console.log(num);
-    if(num=="sButton1"){
-      setIndex(0);
-    }
-    else if(num=="sButton2"){
-      setIndex(1);
-    }
-    else if(num=="sButton3"){
-      setIndex(2);
-    }
-  });
-});
+        console.log("클릭된 버튼의 ID:", event.target.id);
+        num = event.target.id;
+
+        console.log(num);
+        if (num == "sButton1") {
+          setIndex(0);
+        }
+        else if (num == "sButton2") {
+          setIndex(1);
+        }
+        else if (num == "sButton3") {
+          setIndex(2);
+        }
+      });
+    });
 
   }
 
-  const slidSrc = [test,test2,test3]
+  const slidSrc = [test, test2, test3]
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col w-full">
       {/* 상단 헤더 */}
-      <div className="flex items-center ml-10">
-        <a className="flex-shrink-0" href="/">
-          <img className="w-50 h-15" src={Logo} />
+      <div className="flex items-center justify-between p-4">
+        <a href="/">
+          <img className="w-40" src={Logo} alt="Logo" />
         </a>
-        <div className="flex items-center justify-between w-full p-4 bg-white ml-75">
-          <span className="flex h-10 w-130 text-sm border border-gray-300 rounded-full cursor-pointer">
-            <input
-              type="search"
-              name="serch"
-              placeholder="Search"
-              className="w-full px-4 text-sm rounded-full focus:outline-none"
-            />
-          </span>
-          <button onClick={goLogin} type="button" className="py-2  bg-slate-50 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-black w-20 transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg ml-70">
+        <div className="flex-1 flex justify-center">
+          <input
+            type="search"
+            placeholder="Search"
+            className="border rounded-full px-4 py-2 w-40 sm:w-100 focus:outline-none text-base"
+          />
+        </div>
+        <div className="flex items-center space-x-4">
+          <button
+            onClick={() => navigate('/login')}
+            className="px-4 py-2 bg-gray-50 hover:bg-indigo-700 text-black rounded-lg text-base"
+          >
             로그인
           </button>
-          <button onClick={goRegister} type="button" className="py-2  bg-gray-900 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-25 transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mr-20">
+          <button
+            onClick={() => navigate('/register')}
+            className="px-4 py-2 bg-gray-900 hover:bg-indigo-700 text-white rounded-lg text-base"
+          >
             회원가입
           </button>
         </div>
       </div>
 
+
       {/* 내비게이션 바 */}
-      <nav className="bg-black dark:bg-gray-800 shadow">
-        <div className="px-8 mx-auto max-w-7xl">
-          <div className="flex items-center justify-between h-16">
-            <div>
-              <div className="flex items-baseline ml-10 space-x-4">
+      <nav className="flex-1 bg-black text-gray-300 p-4 md:flex justify-center text-lg">
+        <div className="flex justify-between w-full max-w-screen-xlg mx-auto px-4 md:px-10 lg:px-20 xl:px-32">
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">차량구매</a>
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">부속부품</a>
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">견적</a>
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">차량정보</a>
-                {/* <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">견적</a> */}
                 <a className="text-gray-300 hover:text-yellow-200 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">고객지원</a>
-              </div>
-            </div>
-          </div>
         </div>
       </nav>
       {/* 슬라이드 부분 */}
-      
+
       <div class="relative w-full">
         <img src={slidSrc[index]} class="w-full h-160 rounded-lg" alt="Test" />
 
         <div className='slidButton' class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-          <button id="sButton1" onClick={sliderButton}  class="bg-gray-800 rounded-full w-4 h-4"></button>
+          <button id="sButton1" onClick={sliderButton} class="bg-gray-800 rounded-full w-4 h-4"></button>
           <button id="sButton2" onClick={sliderButton} class="bg-gray-800 rounded-full w-4 h-4"></button>
           <button id="sButton3" onClick={sliderButton} class="bg-gray-800 rounded-full w-4 h-4"></button>
         </div>
@@ -101,17 +92,18 @@ const MainPage = () => {
 
       {/*로고 부분*/}
 
-            <div className= "flex flex-row h-screen" >
-            <img alt="blog photo" src={hyundai} class="w-40"/> 
-            <img alt="blog photo" src={hyundai} class="w-40"/> 
-            <img alt="blog photo" src={hyundai} class="w-40"/> 
-            <img alt="blog photo" src={hyundai} class="w-40"/> 
-            <img alt="blog photo" src={hyundai} class="w-40"/> 
-            <img alt="blog photo" src={hyundai} class="w-40"/> 
+      <div className="flex flex-row relative w-full" >
+        <img alt="blog photo" src={hyundai} onClick={() => navigate("/hyundai")} class="w-40" />
+        <img alt="blog photo" src={hyundai} class="w-40" />
+        <img alt="blog photo" src={hyundai} class="w-40" />
+        <img alt="blog photo" src={hyundai} class="w-40" />
+        <img alt="blog photo" src={hyundai} class="w-40" />
+        <img alt="blog photo" src={hyundai} class="w-40" />
 
-            </div> 
-                  
-            
+      </div>
+
+
+
 
 
 
