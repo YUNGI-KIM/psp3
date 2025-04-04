0d93ddb14de625429d36b25c55681a1c072f17d0
Wasppss <lch9243@gmail.com>
2025-04-04 15:20:23 +0900

    고객지원 변경함 2

--- src/Login/Support.js
+++ src/Login/Support.js
@@ -3,7 +3,9 @@ import { useNavigate } from "react-router-dom";
 import Logo from '../Image/logo2.png';
 
 function Support(){
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
+    
+    
     return (
         
         <div className="space-y-2 bg-gray-200 "> 
@@ -31,15 +33,21 @@ function Support(){
                 </label>
                 <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="휴대 전화, - 생략"/>
             </div>
-            <div class=" relative mr-33">
-                <label for="name-with-label" class="px-10 text-gray-700">
+            <div class="flex items-start ml-131 mb-4">
+                <label for="message" class="w-40 text-gray-700 text-base mr-4 pt-11">
                     상담 문의 내용 입력
                 </label>
-                <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-30 px-6 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="문의하기"/>
-            </div>
+                <textarea
+                    id="message"
+                    name="message"
+                    placeholder="문의하기"
+                    rows="4"
+                    class="rounded-lg border border-gray-300 px-4 py-3 w-96 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent resize-none"
+                ></textarea>
+                </div>
             <button
                 onClick={() => navigate('/')}
-                className="ml-65 px-4 py-2 bg-gray-200 hover:bg-gray-400 text-black rounded-lg text-base"
+                className="ml-142 px-4 py-2 bg-gray-200 hover:bg-gray-400 text-black rounded-lg text-base"
             >
                 신청
             </button>
