ccf48ebfb2214ddc9041f7ef115babccae85205c
Wasppss <lch9243@gmail.com>
2025-04-04 14:52:41 +0900

    고객지원 변경함

--- src/Login/Support.js
+++ src/Login/Support.js
@@ -1,37 +1,52 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Logo from '../Image/logo2.png';
 
 function Support(){
+    const navigate = useNavigate(); 
     return (
-        <Link to="/Support">
-    <div className="space-y-4"> 
-        <div>고객지원</div>
-        <div>1:1 상담 신청하기</div>
-        <div class=" relative ">
-            <label for="name-with-label" class="px-5 text-gray-700">
-                이름
-            </label>
-            <input type="text" id="name-with-label" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="이름"/>
-        </div>
-        <div class=" relative ">
-            <label for="name-with-label" class="px-5 text-gray-700">
-                이메일
-            </label>
-            <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="E-mail"/>
-        </div>
-        <div class=" relative ">
-            <label for="name-with-label" class="px-5 text-gray-700">
-                휴대 전화
-            </label>
-            <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="휴대 전화"/>
-        </div>
-        <div class=" relative ">
-            <label for="name-with-label" class="px-5 text-gray-700">
-                상담 신청 정보 입력
-            </label>
-            <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-20 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="제목"/>
-        </div>
-    </div>  
-        </Link>
+        
+        <div className="space-y-2 bg-gray-200 "> 
+            <div className="flex items-center justify-between p-4">
+            <a href="/">
+            <img className="w-40" src={Logo} alt="Logo" />
+            </a></div>
+            <div class= "text-center text-2xl font-black">고객지원</div>
+            <div>1:1 상담 신청하기</div>
+            <div class=" relative ">
+                <label for="name-with-label" class="px-5 text-gray-700">
+                    이름
+                </label>
+                <input type="text" id="name-with-label" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="이름"/>
+            </div>
+            <div class=" relative mr-4">
+                <label for="name-with-label" class="px-5 text-gray-700">
+                    이메일
+                </label>
+                <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="E-mail"/>
+            </div>
+            <div class=" relative mr-9">
+                <label for="name-with-label" class="px-5 text-gray-700">
+                    휴대 전화
+                </label>
+                <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="휴대 전화, - 생략"/>
+            </div>
+            <div class=" relative mr-33">
+                <label for="name-with-label" class="px-10 text-gray-700">
+                    상담 문의 내용 입력
+                </label>
+                <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-30 px-6 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="문의하기"/>
+            </div>
+            <button
+                onClick={() => navigate('/')}
+                className="ml-65 px-4 py-2 bg-gray-200 hover:bg-gray-400 text-black rounded-lg text-base"
+            >
+                신청
+            </button>
+
+        </div> 
+
+
     );
 }
 
