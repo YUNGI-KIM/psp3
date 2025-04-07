434d2daae5c61623d32be1f4645f693e167233a6
Wasppss <lch9243@gmail.com>
2025-04-07 15:32:49 +0900

    고객지원 간격 오류 수정

--- src/Login/Support.js
+++ src/Login/Support.js
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Logo from '../Image/logo2.png';
 
@@ -47,26 +47,26 @@ function Support(){
         <div className="w-full h-full bg-gray-200 absolute space-y-2 pt-10"> 
             
             <div class= "text-center text-2xl font-black">고객지원</div>
-            <div>1:1 상담 신청하기</div>
-            <div class=" relative ">
+            <div class= "text-center">1:1 상담 신청하기</div>
+            <div class=" relative text-center">
                 <label for="name-with-label" class="px-5 text-gray-700">
                     이름
                 </label>
                 <input type="text" id="name-with-label" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="이름"/>
             </div>
-            <div class=" relative mr-4">
+            <div class=" relative text-center mr-5">
                 <label for="name-with-label" class="px-5 text-gray-700">
                     이메일
                 </label>
                 <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="E-mail"/>
             </div>
-            <div class=" relative mr-9">
+            <div class=" relative text-center mr-10">
                 <label for="name-with-label" class="px-5 text-gray-700">
                     휴대 전화
                 </label>
                 <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-100px py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" name="email" placeholder="휴대 전화, - 생략"/>
             </div>
-            <div class="flex items-start ml-129 mb-4">
+            <div class="flex items-start ml-127 mb-4">
                 <label for="message" class="w-40 text-gray-700 text-base mr-4 pt-11">
                     상담 문의 내용 입력
                 </label>
@@ -80,7 +80,7 @@ function Support(){
                 </div>
             <button
                 onClick={() => navigate('/')}
-                className="ml-142 px-4 py-2 bg-gray-200 hover:bg-gray-400 text-black rounded-lg text-base"
+                className="ml-183 px-4 py-2 bg-gray-200 hover:bg-gray-400 text-black rounded-lg text-base"
             >
                 신청
             </button>
@@ -88,7 +88,7 @@ function Support(){
         </div> 
     </div>
 
-    );
+    )
 }
 
 export default Support;
