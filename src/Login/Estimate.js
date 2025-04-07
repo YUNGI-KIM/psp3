7e84bd48f369913aa41e0271e1dda41c666555ba
my <hslee9275@gmail.com>
2025-04-07 17:23:17 +0900

    commit

--- src/Login/Estimate.js
+++ src/Login/Estimate.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Logo from '../Image/logo2.png';
 import sonataInterior from '../Image/Estimate/Hyundai/Sonata/sonataInterior.png';
