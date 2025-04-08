660a0f8481358bcfe1fb21dac628ffa07758e643
my <hslee9275@gmail.com>
2025-04-08 14:38:20 +0900

    LoginPage modified 1. Up to date Logo for container top 2. Lot of Buttons Padding Rebalancing

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import Logo from "../Image/logo2.png";
 
 
 function Login() {
     return (
 
         <div class="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:mt-60 sm:overflow-hidden">
-            <div class="px-4 py-8 sm:px-10">
-                <div class="relative mt-6">
+            <img className="pl-6 pt-1"  src={Logo}/>
+            <div class="px-4 py-2 sm:px-10">
+                <div class="relative">
                     <div class="absolute inset-0 flex items-center">
                         <div class="w-full border-t border-gray-300">
                         </div>
@@ -17,7 +19,7 @@ function Login() {
                         </span>
                     </div>
                 </div>
-                <div class="mt-6">
+                <div class="mt-3">
                     <div class="w-full space-y-6">
                         <div class="w-full">
                             <div class=" relative ">
@@ -31,7 +33,7 @@ function Login() {
                         </div>
 
                         <div>
-                            <span class="block w-full rounded-md shadow-sm">
+                            <span class="block w-full rounded-md ">
                                 <Link to="/">
                                     <button type="button" class="py-2 px-4 bg-black hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-3xl ">
                                         Login!
@@ -39,10 +41,8 @@ function Login() {
                                 </Link>
                             </span>
                         </div>
-
-
                         <div>
-                            <span class="block w-full rounded-md shadow-sm">
+                            <span class="pb-2 block w-full rounded-md ">
                                 <Link to="/register">
                                     <button type="button" class="py-2 px-4 bg-black hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-3xl ">
                                         Resister
@@ -55,7 +55,7 @@ function Login() {
                     </div>
                 </div>
             </div>
-            <div class="px-4 py-6 border-t-2 border-gray-200 bg-gray-50 sm:px-10">
+            <div class="px-4 py-4 border-t-2 border-gray-200 bg-gray-50 sm:px-10">
                 <p class="text-xs leading-5 text-gray-500">
                     This data are test
                 </p>
