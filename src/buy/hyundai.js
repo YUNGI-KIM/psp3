bd4dcc43c3437882eaab108847d7e365a33f0f7f
YUNGI-KIM <kimyungi0101@kakao.com>
2025-04-11 11:12:02 +0900

    Changed Structure in PricingCard

--- src/buy/hyundai.js
+++ src/buy/hyundai.js
@@ -1,5 +1,6 @@
 import DropDown from "./functions/DropDown";
 import Header from "./functions/Header";
+import PricingCard from "./functions/PricingCard";
 import ProductCatalog from "./functions/PricingCard";
 function Hyundai() {
     return (
