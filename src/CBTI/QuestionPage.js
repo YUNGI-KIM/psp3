79a8cb781181093ce539529f07db449e6a2b475e
my <hslee9275@gmail.com>
2025-05-08 20:52:30 +0900

    Update CBTIResult Distplay

--- src/CBTI/QuestionPage.js
+++ src/CBTI/QuestionPage.js
@@ -8,19 +8,19 @@ function QuestionPage() {
 
   const questions = [
     { num: "질문 1번", text: "운전 중 운전자 보조 시스템을 자주 사용하시나요?", progress: "7%", yes: "tech", no: "basic" },
-    { num: "질문 2번", text: "장거리 운행이 많으신가요?", progress: "14%", yes: "comfort", no: "sports" },
-    { num: "질문 3번", text: "SUV를 선호하시나요?", progress: "21%", yes: "suv", no: "sedan" },
+    { num: "질문 2번", text: "장거리 운행이 많으신가요?", progress: "14%", yes: "comfort", no: "athletic" },
+    { num: "질문 3번", text: "SUV를 선호하시나요?", progress: "21%", yes: "utility", no: "sedan" },
     { num: "질문 4번", text: "유지보수가 쉬운 차량을 원하시나요?", progress: "28%", yes: "korean", no: "foreign" },
-    { num: "질문 5번", text: "고속도로 주행이 잦은 편인가요?", progress: "35%", yes: "comfort", no: "sports" },
+    { num: "질문 5번", text: "고속도로 주행이 잦은 편인가요?", progress: "35%", yes: "comfort", no: "athletic" },
     { num: "질문 6번", text: "터치스크린, 무선 업데이트 같은 최신 기능이 중요하신가요?", progress: "42%", yes: "tech", no: "basic" },
-    { num: "질문 7번", text: "오프로드 환경에서도 운전하는 일이 있나요?", progress: "49%", yes: "suv", no: "sedan" },
+    { num: "질문 7번", text: "오프로드 환경에서도 운전하는 일이 있나요?", progress: "49%", yes: "utility", no: "sedan" },
     { num: "질문 8번", text: "국산 차량 정비소를 자주 이용하십니까?", progress: "56%", yes: "korean", no: "foreign" },
-    { num: "질문 9번", text: "주행 시 차량 소음에 민감하신가요?", progress: "63%", yes: "comfort", no: "sports" },
-    { num: "질문 10번", text: "SUV가 도심 주행에도 적합하다고 생각하시나요?", progress: "70%", yes: "suv", no: "sedan" },
+    { num: "질문 9번", text: "주행 시 차량 소음에 민감하신가요?", progress: "63%", yes: "comfort", no: "athletic" },
+    { num: "질문 10번", text: "SUV가 도심 주행에도 적합하다고 생각하시나요?", progress: "70%", yes: "utility", no: "sedan" },
     { num: "질문 11번", text: "국산 브랜드의 가성비를 신뢰하십니까?", progress: "77%", yes: "korean", no: "foreign" },
     { num: "질문 12번", text: "스마트 크루즈 컨트롤 기능을 자주 사용하시나요?", progress: "84%", yes: "tech", no: "basic" },
     { num: "질문 13번", text: "국산 브랜드 차량을 원하시나요?", progress: "91%", yes: "korean", no: "foreign" },
-    { num: "질문 14번", text: "승차감이 중요한 요소라고 생각하시나요?", progress: "100%", yes: "comfort", no: "sports" },
+    { num: "질문 14번", text: "승차감이 중요한 요소라고 생각하시나요?", progress: "100%", yes: "comfort", no: "athletic" },
   ];
 
   const current = questions[index];
