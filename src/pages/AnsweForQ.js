c92d723e2d249bdc2f39eda1e1dbcea9f4af136b
my <hslee9275@gmail.com>
2025-05-12 15:03:55 +0900

    update : update AnsweForQ page

--- src/pages/AnsweForQ.js
+++ src/pages/AnsweForQ.js
@@ -1,64 +1,18 @@
-import Header from "../buy/functions/Header"
-import AdminSupportFn from "../buy/functions/AdminSupportFn";
 
-function receviedSupport() {
 
-    return (
-        <div>
-            {Header()}
-            <div class="container max-w-3xl px-4 mx-auto sm:px-8">
-                <div class="py-8">
-                    <div class="flex flex-row justify-between w-full mb-1 sm:mb-0">
-                        <h2 class="text-2xl leading-tight">
-                            Admin Support Session
-                        </h2>
-                        <div class="text-end">
-                            <form class="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
-                                <div class=" relative ">
-                                    <input type="text" id="&quot;form-subscribe-Filter" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
-                                </div>
-                                <button class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                                    Search
-                                </button>
-                            </form>
-                        </div>
-                    </div>
+function AnswerForQ(){
 
-                    <div class="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
-                        <div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
-                            <table class="min-w-full leading-normal">
-                                <thead>
-                                    <tr>
-                                        <th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800  bg-white border-b border-gray-200  hidden md:table-cell">
-                                            User
-                                        </th>
-                                        <th scope="col" class="px-5 py-3 text-sm font-normal text-left text-gray-800  bg-white border-b border-gray-200  hidden md:table-cell">
-                                            Requset at
-                                        </th>
-                                        <th scope="col" class="px-5 py-3 sm:px-2 sm:py-1 sm:text-sm text-sm font-normal text-left text-gray-800  bg-white border-b border-gray-200 ">
-                                            Title
-                                        </th>
-                                        <th scope="col" class="px-5 py-3 sm:px-2 sm:py-1 sm:text-sm text-sm font-normal text-right text-gray-800  bg-white border-b border-gray-200 ">
-                                            Status
-                                        </th>
-                                        <th scope="col" class="px-5 py-3 sm:px-2 sm:py-1 sm:text-sm text-sm font-normal text-right text-gray-800  bg-white border-b border-gray-200 ">
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
 
-                                    {AdminSupportFn(0)}
-                                    {AdminSupportFn(1)}
-                                    {AdminSupportFn(2)}
-                                    {AdminSupportFn(3)}
+const questions=[
+    { name: "user", requestDate: "2025/04/12", title: "테스트 제목입니다",detail: " 지금 이 문장은 자동으로 엔터가 쳐지는지 테스트를 하기 위해 있는 문장이니 수정을 엄금합니다."      , status: 1 },
+    { name: "aaaa", requestDate: "2025/05/02", title: "receivec", status: 0 },
+    { name: "bbbb", requestDate: "2025/04/30", title: "asdf", status: 0 },
+    { name: "cccc", requestDate: "2025/05/01", title: "집에 보내주세요", status: 1 }
 
-                                </tbody>
-                            </table>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-} export default receviedSupport;
+
+
+]
+return(
+
+);}
