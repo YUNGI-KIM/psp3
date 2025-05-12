244fc1468145b4edf885f210bb2261d272b84583
my <hslee9275@gmail.com>
2025-05-12 11:58:35 +0900

    modified : All CBTI pages  Respoensive web desgin

--- src/buy/functions/AdminSupportFn.js
+++ src/buy/functions/AdminSupportFn.js
@@ -3,12 +3,12 @@ function AdminSupportFn(i) {
         {
             word: "Incomplete",
             color: "absolute inset-0 bg-red-200 rounded-full opacity-50",
-            text: "relative inline-block px-3 py-1 font-semibold leading-tight text-red-900"
+            text: "relative inline-block px-1.5 py-0.5 text-xs sm:px-3 sm:py-1 sm:text-sm md:px-4 md:py-1.5 md:text-base font-semibold leading-tight text-red-900"
         },
         {
             word: "Completed",
             color: "absolute inset-0 bg-green-200 rounded-full opacity-50",
-            text: "relative inline-block px-3 py-1 font-semibold leading-tight text-green-900"
+            text: "relative inline-block px-1.5 py-0.5 text-xs sm:px-3 sm:py-1 sm:text-sm md:px-4 md:py-1.5 md:text-base font-semibold leading-tight text-green-900"
         }
     ];
 
@@ -40,14 +40,14 @@ function AdminSupportFn(i) {
             </td>
 
             {/* 제목 (항상 보이게) */}
-            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+            <td className="px-3 py-4.5 text-xs sm:px-4 sm:py-3 sm:text-sm md:px-5 md:py-5 md:text-base text-sm bg-white border-b border-gray-200">
                 <p className="text-gray-900 whitespace-no-wrap">
                     {Data[i].title}
                 </p>
             </td>
 
             {/* 상태 */}
-            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200 hidden md:table-cell">
+            <td className="py-2 text-xs sm:px-4 sm:py-3 sm:text-sm md:px-5 md:py-5 md:text-base bg-white border-b border-gray-200">
                 <span className={status[Data[i].status].text}>
                     <span aria-hidden="true" className={status[Data[i].status].color}></span>
                     <span className="relative">
@@ -57,7 +57,7 @@ function AdminSupportFn(i) {
             </td>
 
             {/* 답변 링크 (항상 보이게) */}
-            <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+            <td className="py-2 text-xs sm:px-4 sm:py-3 sm:text-sm md:px-5 md:py-5 md:text-base bg-white border-b border-gray-200">
                 <a href="#" className="text-indigo-600 hover:text-indigo-900">
                     Answer
                 </a>
