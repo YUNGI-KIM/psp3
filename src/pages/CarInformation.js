1f9f0faed682e30f1de1751e9b726b50989f8292
my <hslee9275@gmail.com>
2025-05-20 21:29:06 +0900

    modified CarInformation.js Buttons Design more nice

--- src/pages/CarInformation.js
+++ src/pages/CarInformation.js
@@ -52,12 +52,20 @@ function CarInformation() {
       <div>{Header()}</div>
       <div className="bg-white py-10 px-4 sm:px-8">
         <div className="flex justify-center items-center mb-6 space-x-6">
-          <button onClick={prevBrand} className="text-4xl font-bold">ᐊ</button>
+          <button onClick={prevBrand} className="text-4xl font-bold">
+            <svg xmlns="http://www.w3.org/2000/svg"  fill="#808080" viewBox="0 0 256 512" className="w-10 h-10 cursor-pointer hover:scale-110 transition-transform">
+              <path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"/>
+            </svg>
+          </button>
           <div>
             <h1 className="text-3xl font-bold text-center">{currentBrand} 차량 정보</h1>
             <p className="text-center text-sm text-gray-500">브랜드 간 비교</p>
           </div>
-          <button onClick={nextBrand} className="text-4xl font-bold">ᐅ</button>
+          <button onClick={nextBrand} className="text-4xl font-bold">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#808080" viewBox="0 0 256 512"  className="w-10 h-10 cursor-pointer hover:scale-110 transition-transform">
+              <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/>
+            </svg>
+          </button>
         </div>
 
         <AnimatePresence mode="wait">
