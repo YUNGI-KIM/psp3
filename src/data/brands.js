2d54b44f22956c1bef19644ca5fa32161393931a
raccoon0120-swj <raccoon0120@gmail.com>
2025-05-23 10:46:24 +0900

    BMW_Car_image_Edit

--- src/data/brands.js
+++ src/data/brands.js
@@ -1,4 +1,12 @@
 export const brands = {
   Hyundai: ["Avante", "Sonata", "Grandeur", "Santafe", "Casper", "Porter", "Ioniq6", "Palisade"],
   Kia: ["Bongo", "BongoEv", "Carnival", "Ev3", "Ev4", "Ev6", "K5", "K8", "K9", "Morning", "Ray", "Seltos", "Sorento", "Sprotage"],
+  Chevrolet: [],
+  Renault: [],
+  Kgm: [],
+  Genesis: [],
+  Bmw: ["1_Series", "3_Series", "5_Series", "7_Series", "i5", "i7", "iX1", "iX3", "M4", "X1", "X3", "X5", "X7", "Z4_M40i"],
+  Audi: [],
+  Benz: [],
+  Tesla: [],
 };
