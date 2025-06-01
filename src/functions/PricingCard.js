2449532d4b84ad08f0b6a018f98db8844865a8e9
YUNGI-KIM <kimyungi0101@kakao.com>
2025-06-02 05:53:46 +0900

    fix: Image src path fixed for PricingCard.js

--- src/functions/PricingCard.js
+++ src/functions/PricingCard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {cartImg} from "../Image/etc/cart.png";
+import cartImg from "../Image/etc/cart.png";
 
 // ✅ 카테고리 필터
 function CategoryFilter({ categories, activeCategory, onCategoryChange, showFilter }) {
