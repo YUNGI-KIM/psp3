f47a6e741a12b1d8756db28799ad61e0b8575bd9
YUNGI-KIM <kimyungi0101@kakao.com>
2025-06-02 12:36:51 +0900

    feat: search using brandName

--- src/buy/buy.js
+++ src/buy/buy.js
@@ -19,7 +19,7 @@ function Buy() {
 
             <div className="flex">
                 {/*<ProductCatalog pageType={pageType} showFilter={false} />*/}
-                <ProductCatalog pageType={"자동차"} showFilter={false} />
+                <ProductCatalog pageType={"자동차"} brandInput={brand} showFilter={false} />
             </div>
 
 
