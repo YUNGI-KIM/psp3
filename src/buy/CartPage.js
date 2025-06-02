402de904410cac3b52c432da5e317f96ae0040a6
my <hslee9275@gmail.com>
2025-06-02 21:58:12 +0900

    modified CartPage.js layout

--- src/buy/CartPage.js
+++ src/buy/CartPage.js
@@ -29,26 +29,36 @@ function CartPage() {
                             {cartItems.map((item, index) => (
                                 <motion.li
                                     key={item.name}
-                                    className="flex justify-between items-center bg-white rounded-lg shadow p-4"
+                                    className="flex flex-col items-center bg-white rounded-lg shadow p-4 relative"
                                     initial={{ opacity: 0, scale: 0.9, y: 20 }}
                                     animate={{ opacity: 1, scale: 1, y: 0 }}
                                     exit={{ opacity: 0, scale: 0.8, x: 50 }}
                                     transition={{ duration: 0.3 }}
                                     layout
                                 >
-                                    <span className="font-medium">{item.name}</span>
-                                    <span className="font-medium">
-                                      <img
-                                          src={item.image}
-                                          alt={item.name}
-                                          className="w-14 h-14 object-cover rounded-lg"
-                                      />
-                                    </span>
-                                    <span className="text-indigo-600 font-bold">{item.price}</span>
-                                    <svg onClick={() => remove(item)} className="cursor-pointer hover:text-red-700 hover:scale-110 "
-                                         xmlns="http://www.w3.org/2000/svg" fill="black" width="13" viewBox="0 0 384 512">
+                                    {/* 삭제 버튼 우상단 배치 */}
+                                    <svg
+                                        onClick={() => remove(item)}
+                                        className="cursor-pointer hover:text-red-700 hover:scale-110 absolute top-3 right-3"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="black"
+                                        width="18"
+                                        viewBox="0 0 384 512"
+                                    >
                                         <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/>
                                     </svg>
+
+                                    {/* 이미지 */}
+                                    <img
+                                        src={item.image}
+                                        alt={item.name}
+                                        className="w-124 h-60 object-cover rounded-lg mb-4"
+                                    />
+
+                                    {/* 상품명 */}
+                                    <span className="font-medium text-lg text-center mt-2">{item.name}</span>
+                                    {/* 가격 */}
+                                    <span className="text-indigo-600 font-bold text-center mt-1">{item.price}</span>
                                 </motion.li>
                             ))}
                         </AnimatePresence>
