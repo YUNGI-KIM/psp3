5668765ca694455a0cb657203c23e1d356dd461f
YUNGI-KIM <kimyungi0101@kakao.com>
2025-06-06 01:18:26 +0900

    fix: fixed for Move color select, play button, and guide to be horizontally aligned below the image only on /CarDetail/:name

--- src/CarDetail/VehicleDetail.js
+++ src/CarDetail/VehicleDetail.js
@@ -22,73 +22,7 @@ const modelToColorCodes = {
     HyundaiSantafe: ["A2B", "A2B-4NB", "PB2", "PE2", "RN2", "RS2", "WW2", "WW2-4NB", "WWM-4NB", "YBM", "ZGE"],
     HyundaiSonata: ["A2B", "NY9", "R2P", "T2G", "T4M", "T9M", "W6H", "XB9"],
 };
-const hyundaiColorNames = {
-    // 주요 공통 색상
-    "A2B": "어비스 블랙 펄",
-    "A2B-K": "어비스 블랙 펄 (투톤K)",
-    "A2B-R": "어비스 블랙 펄 (투톤R)",
-    "A5G": "아마존 그레이 메탈릭",
-    "C5G": "쉬머링 실버 메탈릭",
-    "M6T": "메타 블루 펄",
-    "PE2": "플루이드 그레이 메탈릭",
-    "PM2": "플루이드 메탈",
-    "R2P": "얼티메이트 레드 메탈릭",
-    "RRR": "인텐스 블루",
-    "SAW": "세이지 그린",
-    "NGM": "녹턴 그레이 메탈릭",
-    "NY9": "화이트 크림",
-    "T2G": "트위스터 블루 펄",
-    "T4A": "카본 메탈",
-    "T4M": "에어로 실버 매트",
-    "T9M": "트랜스포머 그레이",
-    "VAM": "바이올렛 메탈릭",
-    "W6H": "그래비티 골드 매트",
-    "W6H-K": "그래비티 골드 매트 (투톤K)",
-    "W6H-R": "그래비티 골드 매트 (투톤R)",
-    "XB9": "오션 블루",
-    "M9U": "미스티 그레이 펄",
-    "RTE": "슈팅 스타 그레이 매트",
-    "U3P": "블루 핀 마이크로",
-    "W3T": "비비드 베이지",
-    "Y2T": "아틀라스 화이트",
-    // 팰리세이드 특수 조합 (A/xxx, B/xxx 등)
-    "A/A2B": "어비스 블랙 펄 (A 루프)",
-    "A/CBP": "문라이트 블루 펄 (A 루프)",
-    "A/CRP": "가이아 브라운 펄 (A 루프)",
-    "A/GMP": "쉬머링 실버 메탈릭 (A 루프)",
-    "A/PE2": "플루이드 그레이 메탈릭 (A 루프)",
-    "A/R2T": "오로라 그레이 펄 (A 루프)",
-    "A/WC9": "크림 화이트 펄 (A 루프)",
-    "B/A2B": "어비스 블랙 펄 (B 루프)",
-    "B/CBP": "문라이트 블루 펄 (B 루프)",
-    "B/CRP": "가이아 브라운 펄 (B 루프)",
-    "B/GMP": "쉬머링 실버 메탈릭 (B 루프)",
-    "B/PE2": "플루이드 그레이 메탈릭 (B 루프)",
-    "B/R2T": "오로라 그레이 펄 (B 루프)",
-    "B/WC9": "크림 화이트 펄 (B 루프)",
-    "C/A2B": "어비스 블랙 펄 (C 루프)",
-    "C/CBP": "문라이트 블루 펄 (C 루프)",
-    "C/CRP": "가이아 브라운 펄 (C 루프)",
-    "C/GMP": "쉬머링 실버 메탈릭 (C 루프)",
-    "C/PE2": "플루이드 그레이 메탈릭 (C 루프)",
-    "C/R2T": "오로라 그레이 펄 (C 루프)",
-    "C/R8N": "타이가 브라운 펄 (C 루프)",
-    "C/WC9": "크림 화이트 펄 (C 루프)",
-    // 포터
-    "RVB": "클리어 화이트",
-    "YAW": "블루",
-    "ZV": "에코 그린",
-    // 싼타페
-    "A2B-4NB": "어비스 블랙 펄(블랙 루프)",
-    "PB2": "스톰 블루 펄",
-    "RN2": "익스플로러 카키 매트",
-    "RS2": "카퍼 브라운 메탈릭",
-    "WW2": "티타늄 그레이 매트",
-    "WW2-4NB": "티타늄 그레이 매트(블랙 루프)",
-    "WWM-4NB": "마룬 브라운 펄(블랙 루프)",
-    "YBM": "글레이셔 화이트",
-    "ZGE": "사파이어 블루 펄"
-};
+
 
 function VehicleDetail() {
     const {name} = useParams();
@@ -172,10 +106,7 @@ function VehicleDetail() {
                                 carCode={carCode}
                                 colorCode={selectedColor}
                                 colorOptions={colorCodes}
-                                playing={playing}
-                                setPlaying={setPlaying}
-                                index={viewerIndex}
-                                setIndex={setViewerIndex}
+                                onChangeColor={setSelectedColor}
                                 key={`${carCode}-${selectedColor}`}
                             />
                         </>
